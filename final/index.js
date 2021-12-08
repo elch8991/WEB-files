a9dbd50109949c03785d25f345ee070024f1e01c
elch8991 <elch8991@colorado.edu>
2021-12-08 12:39:17 -0700

    Icon Edits
    Icon edits to the footer

--- final/index.js
+++ final/index.js
@@ -1,6 +1,11 @@
+
+console.log("hello world");
+
 const photos = ['Headshot.png','ov.png'];
 const x = 0;
+
 function new(){
+  console.log("clicked");
   if (x < 4){
     document.getElementById("me").innerHTML = 'photos[x]';
     x =+ 1
