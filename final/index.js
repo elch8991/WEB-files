746f348244e8469cab4578956c7767500ae2f070
elch8991 <elch8991@colorado.edu>
2021-12-08 19:48:34 -0700

    Javascript Adjustments
    Adjustments to make javascript photo changes

--- final/index.js
+++ final/index.js
@@ -1,13 +1,22 @@
 
 console.log("hello world");
 
-const photos = ['Headshot.png','ov.png'];
-const x = 0;
+const photos = ['final/Assets/Headshot.png','final/Assets/hike.JPG', 'final/Assets/zion.JPG','final/Assets/Headshot.png'];
 
-function new(){
-  console.log("clicked");
-  if (x < 4){
-    document.getElementById("me").innerHTML = 'photos[x]';
-    x =+ 1
+// const photos = ['<img id = "theSwap" src="final/Assets/Headshot.png" alt="Photo of Elizabeth Chester." class ="zoomPic"/>', '<img id = "theSwap" src="final/Assets/ov.png" alt="Photo of Elizabeth Chester." class ="zoomPic"/>']
+let x = 0;
+const pSelect = photos[x]
+
+
+document.getElementById("me").addEventListener("click", pSwap);
+
+function pSwap() {
+  console.log(photos[x]);
+  document.getElementById("theSwap").src = photos[x];
+  if (x >= 2){
+    x = 0;
+  } else {
+    x ++;
   }
 }
+// Reference: https://oceanestars.github.io/WebRecitation/Dom-Example/index.html
